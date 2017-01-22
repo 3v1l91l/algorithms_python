f6cf63ee45fd73385aada1b5009fedc293cf6ae4
Misha Zhukov <3v1l91l@gmail.com>
2017-01-22 12:56:29 +0200

    Add implementation of binary search

--- algorithms/search/binary.py
+++ algorithms/search/binary.py
@@ -1,2 +1,24 @@
 def search(searchList, searchItem):
+    """
+    Searches for a given search item in a given search list and
+    returns its index.
+
+    :param searchList: list of integers
+    :param searchItem: integer to search for
+    :return: index of the found item or False if not found
+    """
+
+    highIndex = len(searchList) -1
+    lowIndex = 0
+
+    while lowIndex <= highIndex:
+        midIndex = (highIndex + lowIndex) // 2
+        midItem = searchList[midIndex]
+
+        if searchItem == midItem:
+            return midIndex
+        elif searchItem > midItem:
+            lowIndex = midIndex + 1
+        else:
+            highIndex = midIndex - 1
     return False
--- tests/test_search.py
+++ tests/test_search.py
@@ -10,3 +10,15 @@ class TestBinarySearch(TestCase):
 
         foundIndex = binary.search(self.searchList, -1)
         self.assertIs(foundIndex, False)
+
+    def test_range_with_search_items(self):
+        self.searchList = range(0,10)
+
+        foundIndex = binary.search(self.searchList, 0)
+        self.assertIs(foundIndex, 0)
+
+        foundIndex = binary.search(self.searchList, 5)
+        self.assertIs(foundIndex, 5)
+
+        foundIndex = binary.search(self.searchList, 9)
+        self.assertIs(foundIndex, 9)
