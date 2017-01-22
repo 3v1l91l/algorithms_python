174b8d36063a24560606b93aed8a5612a5dc3332
Misha Zhukov <3v1l91l@gmail.com>
2017-01-22 14:50:10 +0200

    Add selection sort implementation

--- algorithms/sort/selection.py
+++ algorithms/sort/selection.py
@@ -1,2 +1,19 @@
 def sort(sortList):
+    """
+    Sort list item in an ascending order
+
+    :param sortList: list to be sorted
+    :return: sorted list
+    """
+
+    for i in range(0, len(sortList)):
+        iMin = i
+
+        for j in range(i+1, len(sortList)):
+            if sortList[iMin] > sortList[j]:
+                iMin = j
+
+        if sortList[i] != sortList[iMin]:
+            sortList[i], sortList[iMin] = sortList[iMin], sortList[i]
+
     return sortList
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -9,4 +9,11 @@ class TestSelectionSort(TestCase):
         self.sortList = list(range(9, -1, -1))
 
         sortedList = selection.sort(self.sortList)
-        self.assertIs(sortedList, list(range(0, 10)))
+        self.assertEqual(sortedList, list(range(0, 10)))
+
+    def test_list_with_not_unique_items(self):
+        self.sortList = [5,2,1,4,3,2,5];
+
+        sortedList = selection.sort(self.sortList)
+        self.assertEqual(sortedList, [1,2,2,3,4,5,5])
+
